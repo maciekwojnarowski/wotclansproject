41e474a198e7a1e70b78d3d83948c881f2dcaac4
Maciek Wojnarowski <maciek@pizza.pl>
2017-11-05 02:16:35 +0100

    heheheh

--- wotclans/get_data.py
+++ wotclans/get_data.py
@@ -1,7 +1,8 @@
 from .models import Clan, Player
 import requests
+from datetime import datetime
 
-clan = Clan()
+
 player = Player()
 
 
@@ -13,12 +14,22 @@ def get_clans_data():
         clans_data = requests.get(clans_json + str(page_no)).json()
         if clans_data['meta']['count'] > 0:
             for i in range(clans_data['meta']['count']):
+                clan = Clan()
                 clan.full_name = clans_data['data'][i]['name']
-                clan.members_count = clans_data['data'][i]['members_count']
-                clan.color = clans_data['data'][i]['color']
-                clan.created_at = clans_data['data'][i]['created_at']
                 clan.tag = clans_data['data'][i]['tag']
+                clan.motto = clans_data['data'][i]['motto']
                 clan.clan_id = clans_data['data'][i]['clan_id']
+                clan.members_count = clans_data['data'][i]['members_count']
+                clan.created_at = clans_data['data'][i]['created_at']
+                clan.color = clans_data['data'][i]['color']
+                # clan.description = clans_data['data'][i]['tag']
+                # clan.description_html = clans_data['data'][i]['tag']
+                clan.updated_at = datetime.now()
+                clan.elo_gm_X = clans_data['data'][i]['tag']
+                clan.elo_gm_VIII = clans_data['data'][i]['tag']
+                clan.elo_sh_X = clans_data['data'][i]['tag']
+                clan.elo_sh_VIII = clans_data['data'][i]['tag']
+                clan.elo_sh_VI = clans_data['data'][i]['tag']
 
 
 
--- wotclans/models.py
+++ wotclans/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class Clan(models.Model):
     full_name = models.CharField(max_length=50)
     tag = models.CharField(max_length=5)
-    motto = models.CharField(max_length=50, null=True)
+    motto = models.CharField(max_length=50, default='')
     clan_id = models.PositiveIntegerField()
     members_count = models.PositiveSmallIntegerField()
     created_at = models.DateTimeField(null=True)
     color = models.CharField(max_length=12, default='black')
-    description = models.TextField(null=True)
-    description_html = models.TextField(null=True)
+    description = models.TextField(default='')
+    description_html = models.TextField(default='')
     updated_at = models.DateTimeField(null=True)
     elo_gm_X = models.PositiveSmallIntegerField(null=True)
     elo_gm_VIII = models.PositiveSmallIntegerField(null=True)
@@ -21,17 +21,20 @@ class Clan(models.Model):
 
 
 class Player(models.Model):
-    name = models.CharField(max_length=25)
-    account_id = models.PositiveIntegerField()
-    last_updated = models.DateTimeField()
+    nickname = models.CharField(max_length=25)
+    account_id = models.PositiveIntegerField(null=True)
+    last_updated = models.DateTimeField(null=True)
     created_at = models.DateTimeField(null=True)
     last_battle_time = models.DateTimeField(null=True)
     client_language = models.CharField(max_length=5)
     clan_id = models.ForeignKey(Clan)
     logout_at = models.DateTimeField(null=True)
-    statistics_random = models.TextField(null=True)
-    statistics_gm10 = models.TextField(null=True)
-    statistics_gm8 = models.TextField(null=True)
-    statistics_gm6 = models.TextField(null=True)
+    statistics_random = models.TextField(default='')
+    statistics_gm10 = models.TextField(default='')
+    statistics_gm8 = models.TextField(default='')
+    statistics_gm6 = models.TextField(default='')
+    statistics_skirmishes = models.TextField(default='')
+    statistics_sh_defense = models.TextField(default='')
+
 
 
